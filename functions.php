dbe3af13ad07c4044a404b57f4cc699d67f9eeea
Stu Cowley <stucowley@outlook.com>
2020-11-28 11:31:15 +1030

    Init

---
+++ functions.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Functions and definitions
+ * 
+ * @package StuCowley
+ * @subpackage bellezashop
+ * @since 1.0.0
+ */
+
+if ( ! function_exists( 'bellezashop_setup' ) ) {
+  /**
+   * Sets up theme defaults and registers support for various WordPress features
+   * 
+   * @since 1.0.0
+   * @return void
+   */
+  function bellezashop_setup() {
+    
+  }
+}
+add_action( 'after_setup_theme', 'bellezashop_setup' );
