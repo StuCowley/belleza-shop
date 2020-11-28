639ef41bd983be2e4ae68720fc665862305fde2a
Stu Cowley <stucowley@outlook.com>
2020-11-28 11:32:46 +1030

    Add title-tag support

--- functions.php
+++ functions.php
@@ -15,7 +15,12 @@ if ( ! function_exists( 'bellezashop_setup' ) ) {
    * @return void
    */
   function bellezashop_setup() {
-    
+    /**
+     * Let WordPress manage the document title.
+     * This theme does not use a hard-coded <title> tag in the document head, 
+     * WordPress will provide it for us.
+     */
+    add_theme_support( 'title-tag' );
   }
 }
 add_action( 'after_setup_theme', 'bellezashop_setup' );
