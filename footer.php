6abe29a0ae3d4259e8378c1f2fb0a4b30f492b05
Stu Cowley <stucowley@outlook.com>
2020-11-28 11:59:24 +1030

    Init

---
+++ footer.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * The template for displaying the footer
+ * 
+ * Contains the closing of the <footer> and all closing <body> tags
+ * 
+ * @package StuCowley
+ * @subpackage bellezashop
+ * @since 1.0.0
+ */
+
+?>
+
+  <footer>
+    <!-- CONTENTS -->
+  </footer>
+
+  <?php wp_footer(); ?>
+</body>
+</html>
