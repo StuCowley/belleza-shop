860edf55d7d3ab73cffcec1a2e75a1d235d0852f
Stu Cowley <stucowley@outlook.com>
2020-11-28 11:50:22 +1030

    Add enable SVG upload functionality

--- functions.php
+++ functions.php
@@ -56,3 +56,47 @@ if ( ! function_exists( 'bellezashop_setup' ) ) {
   }
 }
 add_action( 'after_setup_theme', 'bellezashop_setup' );
+
+/**
+ * Enable SVG upload
+ * 
+ * Enables the ability to upload images that use the .svg extension. This 
+ * functionality is not compatible with WordPress for some reason, it is 2020 
+ * by the way.
+ * 
+ * @since 1.0.0
+ * @return void
+ */
+
+add_filter( 'wp_check_filetype_and_ext', function($data, $file, $filename, $mimes) {
+  global $wp_version;
+  
+  if ( $wp_version !== '4.7.1' ) {
+    return $data;
+  }
+
+  $filetype = wp_check_filetype( $filename, $mimes );
+
+  return [
+    'ext'             => $filetype['ext'],
+    'type'            => $filetype['type'],
+    'proper_filename' => $data['proper_filename']
+  ];
+}, 10, 4 );
+
+function cc_mime_types( $mimes ){
+  $mimes['svg'] = 'image/svg+xml';
+  return $mimes;
+}
+add_filter( 'upload_mimes', 'cc_mime_types' );
+
+function bellezashop_fix_svg() {
+  echo 
+  '<style type="text/css">
+    .attachment-266x266, .thumbnail img {
+      width: 100% !important;
+      height: auto !important;
+    }
+  </style>';
+}
+add_action( 'admin_head', 'bellezashop_fix_svg' );
