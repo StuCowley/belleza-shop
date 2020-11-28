732acc2c9c6066e14e8f8d0998d1006c8ec4fc67
Stu Cowley <stucowley@outlook.com>
2020-11-28 12:58:51 +1030

    Init

---
+++ template-parts/header/menu-toggle.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Displays the header menu toggle button
+ * 
+ * @package StuCowley
+ * @subpackage bellezashop
+ * @since 1.0.0
+ */
+
+?>
+
+<div class="sc-header-menu-toggle">
+  <button class="sc-header-menu-toggle-btn">
+    <svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+      <title>Menu</title>
+      <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/>
+    </svg>
+  </button>
+</div><!-- /.sc-header-menu-toggle -->
