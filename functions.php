ca74d602a8cda8d2b42aef67df5f4f20dc4fd952
Stu Cowley <stucowley@outlook.com>
2020-11-28 11:36:36 +1030

    Add support for custom logos

--- functions.php
+++ functions.php
@@ -32,6 +32,24 @@ if ( ! function_exists( 'bellezashop_setup' ) ) {
         'footer'  =>  esc_html__( 'Footer menu', 'bellezashop' ),
       )
     );
+
+
+    /**
+     * Add support for the custom logo
+     */
+    $logoWidth = 300;
+    $logoHeight = 100;
+
+    add_theme_support(
+      'custom-logo',
+      array(
+        'height'                => $logoHeight,
+        'width'                 => $logoWidth,
+        'flex-width'            => true,
+        'flex-height'           => true,
+        'unlink-homepage-logo'  => false,
+      )
+    );
   }
 }
 add_action( 'after_setup_theme', 'bellezashop_setup' );
