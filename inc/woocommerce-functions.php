10a1685f59c5e78b42f170f19ce5e1a547062633
Stu Cowley <stucowley@outlook.com>
2020-11-29 23:07:12 +1030

    Remove default WooCommerce styling

--- inc/woocommerce-functions.php
+++ inc/woocommerce-functions.php
@@ -6,3 +6,13 @@
  * @subpackage bellezashop
  * @since 1.0.0
  */
+
+/**
+ * Removes default WooCommerce styling
+ * 
+ * @since 1.0.0
+ * @return void
+ */
+if ( class_exists( 'Woocommerce' ) ) {
+  add_filter( 'woocommerce_enqueue_styles', '__return_empty_array' );
+}
