12c45f51deb3b70425f4c9b3da2eeaec8a38dfa3
Stu Cowley <stucowley@outlook.com>
2020-12-13 10:14:40 +1030

    Init

---
+++ template-parts/footer/site-footer.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Displays the site footer
+ * 
+ * @package StuCowley
+ * @subpackage bellezashop
+ * @since 1.0.0
+ */
+
+?>
+
+<footer class="sc-page-footer">
+  <div class="sc-page-footer-container">
+    <?php get_template_part( 'template-parts/footer/footer-branding' ); ?>
+    <?php get_template_part( 'template-parts/footer/footer-nav' ); ?>
+    <?php get_template_part( 'template-parts/footer/footer-notices' ); ?>
+  </div>
+</footer><!-- /.sc-page-footer -->
