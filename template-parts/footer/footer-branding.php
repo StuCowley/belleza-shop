18aeccd171c8ec45b26dd40db2eca0c1a00c77a3
Stu Cowley <stucowley@outlook.com>
2020-12-13 10:14:46 +1030

    Init

---
+++ template-parts/footer/footer-branding.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Displays the footer site branding.
+ * 
+ * @package StuCowley
+ * @subpackage bellezashop
+ * @since 1.0.0
+ */
---
+++ template-parts/footer/footer-notices.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Displays footer notices, such as copyright dates, site dev info
+ * 
+ * @package StuCowley
+ * @subpackage bellezashop
+ * @since 1.0.0
+ */
