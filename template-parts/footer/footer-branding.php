bd9561286bd19e016e98769d00bca120e5ea3601
Stu Cowley <stucowley@outlook.com>
2020-12-18 18:10:36 +1030

    Init

--- template-parts/footer/footer-branding.php
+++ template-parts/footer/footer-branding.php
@@ -6,3 +6,9 @@
  * @subpackage bellezashop
  * @since 1.0.0
  */
+
+?>
+
+<div class="sc-footer-branding">
+  <img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo-on-black.svg" alt="The white version of the <?php bloginfo( 'name' ) ?> logo" />
+</div><!-- /.sc-footer-branding -->
