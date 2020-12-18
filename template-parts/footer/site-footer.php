3cd20d218fc935869658072d9623328392a2160b
Stu Cowley <stucowley@outlook.com>
2020-12-18 18:00:05 +1030

    Add call to footer-contact.php

--- template-parts/footer/site-footer.php
+++ template-parts/footer/site-footer.php
@@ -12,6 +12,7 @@
 <footer class="sc-page-footer">
   <div class="sc-page-footer-container">
     <?php get_template_part( 'template-parts/footer/footer-branding' ); ?>
+    <?php get_template_part( 'template-parts/footer/footer-contact' ); ?>
     <?php get_template_part( 'template-parts/footer/footer-nav' ); ?>
     <?php get_template_part( 'template-parts/footer/footer-notices' ); ?>
   </div>
