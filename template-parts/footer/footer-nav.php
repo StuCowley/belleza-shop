69df7d0272fd2b99d0a183f6c1ddba741996a14b
Stu Cowley <stucowley@outlook.com>
2020-12-18 18:41:42 +1030

    Init

--- template-parts/footer/footer-nav.php
+++ template-parts/footer/footer-nav.php
@@ -6,3 +6,43 @@
  * @subpackage bellezashop
  * @since 1.0.0
  */
+
+?>
+
+<nav class="sc-footer-nav">
+  <?php
+  /** 
+   * Display the primary footer navigation menu, which shares the header menu
+   * 
+   * @package StuCowley
+   * @subpackage bellezashop
+   * @since 1.0.0
+   */
+
+  $menuDefaults = array(
+    'theme_location'  =>  'header',
+    'container_class' =>  null
+  );
+
+  wp_nav_menu( $menuDefaults );
+  ?>
+</nav><!-- /.sc-footer-nav -->
+
+<nav class="sc-footer-nav">
+<?php
+  /** 
+   * Display the primary footer navigation menu, which shares the header menu
+   * 
+   * @package StuCowley
+   * @subpackage bellezashop
+   * @since 1.0.0
+   */
+
+  $menuDefaults = array(
+    'theme_location'  =>  'policies',
+    'container_class' =>  null
+  );
+
+  wp_nav_menu( $menuDefaults );
+  ?>
+</nav><!-- /.sc-footer-nav -->
