bdfe3ad5f67b28a35faf2cb0e6e9c5c63994de87
Stu Cowley <stucowley@outlook.com>
2020-12-18 19:09:42 +1030

    Init

--- template-parts/footer/footer-notices.php
+++ template-parts/footer/footer-notices.php
@@ -6,3 +6,16 @@
  * @subpackage bellezashop
  * @since 1.0.0
  */
+
+?>
+
+<div class="sc-footer-notices">
+  <div class="sc-footer-copyright">
+    &copy; <?php echo date( 'Y' ); ?> <?php bloginfo( 'name' ); ?>. All rights reserved
+  </div><!-- /.sc-footer-copyright -->
+
+  <div class="sc-footer-author">
+    Design &plus; code by 
+    <a href="https://github.com/StuCowley" target="new">Stu Cowley</a>
+  </div><!-- /.sc-footer-author -->
+</div><!-- /.sc-footer-notices -->
