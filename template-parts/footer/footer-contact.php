a876b5ff758b14adc33971ed491059558c4cc160
Stu Cowley <stucowley@outlook.com>
2020-12-18 18:26:02 +1030

    Init

--- template-parts/footer/footer-contact.php
+++ template-parts/footer/footer-contact.php
@@ -8,3 +8,24 @@
  */
 
 ?>
+
+<div class="sc-footer-contact">
+  <ul class="sc-contact-details">
+    <li>
+      1/10 Cadell Street<br />
+      Goolwa, SA 5214
+    </li>
+
+    <li>
+      <a href="tel:885550255">
+        (08) 8555 0255
+      </a>
+    </li>
+
+    <li>
+      <a href="mailto:<?php bloginfo( 'admin_email' ); ?>">
+        <?php bloginfo( 'admin_email' ); ?>
+      </a>
+    </li>
+  </ul>
+</div><!-- /.sc-footer-contact -->
