8a7994556a4ab2f337f26f5134c625553c174592
Stu Cowley <stucowley@outlook.com>
2020-12-21 14:17:22 +1030

    Delete calls to footer template parts ready for footer re-work

--- template-parts/footer/site-footer.php
+++ template-parts/footer/site-footer.php
@@ -9,14 +9,6 @@
 
 ?>
 
-<footer class="sc-page-footer">
-  <div class="sc-page-footer-container">
-    <div class="sc-footer-grid">
-      <?php get_template_part( 'template-parts/footer/footer-branding' ); ?>
-      <?php get_template_part( 'template-parts/footer/footer-contact' ); ?>
-      <?php get_template_part( 'template-parts/footer/footer-nav' ); ?>
-    </div>
-    
-    <?php get_template_part( 'template-parts/footer/footer-notices' ); ?>
-  </div><!-- /.sc-footer-grid -->
-</footer><!-- /.sc-page-footer -->
+<footer>
+
+</footer>
