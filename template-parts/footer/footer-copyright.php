5dc7bb16c0fbf791d6624797b2dfd97f9c44ac2d
Stu Cowley <stucowley@outlook.com>
2020-12-21 20:55:51 +1030

    Add re-worked footer copyright and author info

---
+++ template-parts/footer/footer-copyright.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Displays the footer copyright and author
+ * 
+ * @package StuCowley
+ * @subpackage bellezashop
+ * @since 1.0.0
+ */
+
+?>
+
+<div class="sc-footer-copyright">
+  <p>
+    &copy;<?php echo date( 'Y' ); ?> <?php bloginfo( 'name' ); ?>. All rights reserved.
+  </p>
+
+  <p>
+    Design &amp; code by Stu Cowley.
+  </p>
+</div>
