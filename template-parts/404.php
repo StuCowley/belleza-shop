de5654b6a12c752b8d36276007d22c651025b629
Stu Cowley <stucowley@outlook.com>
2021-02-06 20:32:20 +1030

    Init

---
+++ template-parts/404.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Template for display 404 pages (not found)
+ * 
+ * @package StuCowley
+ * @subpackage bellezashop
+ * @since 1.0.0
+ */
+
+get_header();
+?>
+
+<div class="sc-page-content sc-404-page">
+
+</div><!-- /.sc-page-content -->
+
+<?php
+get_footer();
