a54884676bd39dcf82f6bb9fd0df290836a5b048
Stu Cowley <stucowley@outlook.com>
2021-02-19 18:01:05 +1030

    Add content

--- 404.php
+++ 404.php
@@ -11,7 +11,16 @@ get_header();
 ?>
 
 <div class="sc-page-content sc-404-page">
-  404
+  <h1>404</h1>
+  <h2>Page not found</h2>
+
+  <p>
+    Oops! The page you were looking for does not exist. It might have been moved or deleted.
+  </p>
+
+  <a href="<?php bloginfo( 'url' ); ?>">
+    Back to home
+  </a>
 </div><!-- /.sc-page-content -->
 
 <?php
